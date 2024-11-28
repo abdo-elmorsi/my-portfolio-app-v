727c199fb385e4ff963e528b30b578ce8aa419c1
abdo-elmorsi <abdelrahmandiv@gmail.com>
2024-11-28 20:11:07 +0200

    test remote

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -21,7 +21,7 @@ const HomeScreen = () => {
 		`/users/${USERDATA.githubUser}`,
 		{
 			staleTime: 1000 * 60 * 5, // 5 minutes
-			cacheTime: 1000 * 60 * 30, // 30 minutes
+			cacheTime: 1000 * 60 * 20, // 20 minutes
 			refetchOnWindowFocus: false,
 		}
 	);
